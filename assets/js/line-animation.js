446b6161e4ac94e152f56820ab7f4b335036ccea
abdur-shobur <abdurshobur.developer@gmail.com>
2023-04-17 12:27:02 +0600

    update login reg page

--- assets/js/line-animation.js
+++ assets/js/line-animation.js
@@ -1,26 +1,40 @@
-//  final 1
-window.addEventListener('scroll', function () {
-	let l = Path_440.getTotalLength();
+// Get a reference to the <path>
+var path = document.querySelector('#star-path');
 
-	let dasharray = l;
-	let dashoffset = l;
-	e = document.documentElement;
-	// if (e.scrollTop === 1000) {
-	// }
-	theFill.setAttributeNS(null, 'stroke-dasharray', l);
-	theFill.setAttributeNS(null, 'stroke-dashoffset', l);
-	dashoffset = l - (window.scrollY * l) / (e.scrollHeight - e.clientHeight);
-	console.log(
-		'window.scrollY',
-		window.scrollY,
-		'scrollTop',
-		e.scrollTop,
-		'scrollHeight',
-		e.scrollHeight,
-		'clientHeight',
-		e.clientHeight,
-		'dash-offset',
-		dashoffset
-	);
-	theFill.setAttributeNS(null, 'stroke-dashoffset', dashoffset);
+// Get length of path... ~577px in this case
+var pathLength = path.getTotalLength();
+
+// Make very long dashes (the length of the path itself)
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+// Offset the dashes so the it appears hidden entirely
+path.style.strokeDashoffset = pathLength;
+
+// Jake Archibald says so
+// https://jakearchibald.com/2013/animated-line-drawing-svg/
+path.getBoundingClientRect();
+
+// When the page scrolls...
+window.addEventListener('scroll', function (e) {
+	// What % down is it?
+	// https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+	// Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+	var scrollPercentage =
+		(document.documentElement.scrollTop + document.body.scrollTop) /
+		(document.documentElement.scrollHeight -
+			document.documentElement.clientHeight);
+
+	// Length to offset the dashes
+	var drawLength = pathLength * scrollPercentage;
+
+	// Draw in reverse
+	path.style.strokeDashoffset = pathLength - drawLength;
+
+	// When complete, remove the dash array, otherwise shape isn't quite sharp
+	// Accounts for fuzzy math
+	if (scrollPercentage >= 0.99) {
+		path.style.strokeDasharray = 'none';
+	} else {
+		path.style.strokeDasharray = pathLength + ' ' + pathLength;
+	}
 });
