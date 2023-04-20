7753d99ef118c3539033cedaa7a82753fea9f0a1
abdur-shobur <abdurshobur.developer@gmail.com>
2023-04-20 16:38:02 +0600

    add online all

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let coverScreen = document.getElementById('coverScreen');
+let body = document.getElementsByTagName('body');
+document.addEventListener('readystatechange', function (event) {
+	if (document.readyState === 'complete') {
+		coverScreen.style.display = 'none';
+		body.style.display = 'block';
+	}
+});
