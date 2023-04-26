eda5afcbf060dc85787e7df2666e3469f1cd45ce
abdur-shobur <abdurshobur.developer@gmail.com>
2023-04-26 11:37:49 +0600

    add ask how work responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-let coverScreen = document.getElementById('coverScreen');
-let body = document.getElementsByTagName('body');
-document.addEventListener('readystatechange', function (event) {
-	if (document.readyState === 'complete') {
-		coverScreen.style.display = 'none';
-		body.style.display = 'block';
-	}
-});
+// let coverScreen = document.getElementById('coverScreen');
+// let body = document.getElementsByTagName('body');
+// document.addEventListener('readystatechange', function (event) {
+// 	if (document.readyState === 'complete') {
+// 		coverScreen.style.display = 'none';
+// 		body.style.display = 'block';
+// 	}
+// });
+
+window.onload = function () {
+	document.querySelector('.preloader').style.display = 'none';
+};
