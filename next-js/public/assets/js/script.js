51b75edb0543a7494053dcb56ebaaf36790bf856
abdur-shobur <abdurshobur.developer@gmail.com>
2025-01-25 19:10:02 +0600

    only html

--- next-js/public/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// let coverScreen = document.getElementById('coverScreen');
-// let body = document.getElementsByTagName('body');
-// document.addEventListener('readystatechange', function (event) {
-// 	if (document.readyState === 'complete') {
-// 		coverScreen.style.display = 'none';
-// 		body.style.display = 'block';
-// 	}
-// });
